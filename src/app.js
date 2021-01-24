28fc85381bc4bba2cb1a8fa1807bcc800754b5fe
jatinvyas1 <jatin.vyas@outlook.com>
2021-01-24 17:38:32 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -4,20 +4,30 @@ const app = express()
 
 
 // Your code goes here
+const subscriber = require("./models/subscribers");
+
+app.get("/subscribers",(req,res)=>{
+    subscriber.find().then((ans)=>{
+        res.send(ans);
+    });
+})
 
 
+app.get("/subscribers/name",(req,res)=>{
+    subscriber.find().select({"name":1,"subscribedChannel":1}).then(ans=>{
+        res.send(ans);
+    })
+})
 
+app.get("/subscribers/:id",(req,res)=>{
+    subscriber.find({id}).then(ans=>{
+        if(!ans){
+            res.sendStatus(400).send("message: error.message");
+        }
+        res.send(ans);
+    })
 
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
